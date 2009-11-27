54e15192d133b01ef01c38e6c094556c83ada025
Lukas Zdych <lukas.zdych@gmail.com>
2009-11-27 22:05:12 +0000

    - reimported previously removed package (by mistake)

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a2'
+
+setup(name='collective.blogging',
+      version=version,
+      description="A blogging extension for Plone.",
+      long_description=open(os.path.join("collective", "blogging", "README.txt")).read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Lukas Zdych',
+      author_email='lukas.zdych@gmail.com',
+      url='http://plone.org/products/collective.blogging',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'Plone',
+          'Products.ATContentTypes',
+          'archetypes.schemaextender',
+          'archetypes.markerfield',
+          'plone.app.contentmenu',
+          'plone.app.contentrules',
+          'plone.app.form',
+          'plone.memoize',
+          'plone.app.jquerytools',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins = ["ZopeSkel"],
+      )
