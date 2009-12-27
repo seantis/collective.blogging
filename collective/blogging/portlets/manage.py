a4218ea24f0e7bd398d5f91fed048edfca4f269a
Lukas Zdych <lukas.zdych@gmail.com>
2009-12-27 01:09:12 +0000

    - added pictures folder reference to manage blog portlet

--- collective/blogging/portlets/manage.py
+++ collective/blogging/portlets/manage.py
@@ -10,6 +10,7 @@ from plone.app.portlets.portlets import base
 from plone.app.form.widgets.uberselectionwidget import UberSelectionWidget
 from plone.memoize.instance import memoize
 
+from Products.ATContentTypes.interface.folder import IATFolder, IATBTreeFolder
 from Products.CMFPlone import PloneMessageFactory as PMF
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -54,6 +55,16 @@ class IManagePortlet(IPortletDataProvider):
             default_query='path:'
         )
     )
+    
+    target_pictures = schema.Choice(
+        title=_(u"Pictures"),
+        description=_(u"Find an user defined folder for blog pictures and photo galleries."),
+        required=False,
+        source=SearchableTextSourceBinder(
+            {'object_provides' : [IATFolder.__identifier__, IATBTreeFolder.__identifier__]},
+            default_query='path:'
+        )
+    )
 
 class Assignment(base.Assignment):
     """Portlet assignment.
@@ -67,11 +78,14 @@ class Assignment(base.Assignment):
     target_blog = None
     addable_types = ()
     target_drafts = None
+    target_pictures = None
     
-    def __init__(self, target_blog=None, addable_types=(), target_drafts=None):
+    def __init__(self, target_blog=None, addable_types=(),
+                    target_drafts=None, target_pictures=None):
         self.target_blog = target_blog
         self.addable_types = addable_types
         self.target_drafts = target_drafts
+        self.target_pictures = target_pictures
     
     @property
     def title(self):
@@ -114,6 +128,12 @@ class Renderer(base.Renderer):
         drafts = self.drafts()
         if drafts is not None:
             return drafts.absolute_url()
+    
+    @property
+    def pictures_url(self):
+        pictures = self.pictures()
+        if pictures is not None:
+            return pictures.absolute_url()
     
     @property
     def creation_links(self):
@@ -173,6 +193,25 @@ class Renderer(base.Renderer):
         if HAS_LINGUA_PLONE:
             return obj.getTranslation()
         return obj
+    
+    @memoize
+    def pictures(self):
+        """ Get the pictures folder the portlet is pointing to """
+        
+        pictures_path = self.data.target_pictures
+        if not pictures_path:
+            return None
+
+        if pictures_path.startswith('/'):
+            pictures_path = pictures_path[1:]
+        
+        if not pictures_path:
+            return None
+        portal = self.portal_state.portal()
+        obj = portal.restrictedTraverse(pictures_path, default=None)
+        if HAS_LINGUA_PLONE:
+            return obj.getTranslation()
+        return obj
 
     @property
     def portal_url(self):
@@ -192,6 +231,7 @@ class AddForm(base.AddForm):
     form_fields = form.Fields(IManagePortlet)
     form_fields['target_blog'].custom_widget = UberSelectionWidget
     form_fields['target_drafts'].custom_widget = UberSelectionWidget
+    form_fields['target_pictures'].custom_widget = UberSelectionWidget
     
     label = _(u"Add Manage Blog portlet")
     description = _(u"This portlet helps to manage blog's content.")
@@ -209,6 +249,7 @@ class EditForm(base.EditForm):
     form_fields = form.Fields(IManagePortlet)
     form_fields['target_blog'].custom_widget = UberSelectionWidget
     form_fields['target_drafts'].custom_widget = UberSelectionWidget
+    form_fields['target_pictures'].custom_widget = UberSelectionWidget
 
     label = _(u"Edit Manage Blog portlet")
     description = _(u"This portlet helps to manage blog's content.")
