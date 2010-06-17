df38d14d2d0500a026a39f78b643d9bc35f15eba
Kees Hink <keeshink@gmail.com>
2010-06-17 12:22:23 +0000

    Fix installation in Plone4.0b4 (refs #13 http://plone.org/products/collective.blogging/issues/13)

--- collective/blogging/setuphandlers.py
+++ collective/blogging/setuphandlers.py
@@ -91,7 +91,7 @@ def setupViews(context):
     for ptype in BLOG_TYPES:
         for view_method in BLOG_VIEWS:
             type_info = portal_types.getTypeInfo(ptype)
-            if view_method not in type_info.view_methods:
+            if type_info and view_method not in type_info.view_methods:
                 type_info.view_methods = type_info.view_methods + (view_method,)
                 log.info('"%s" view installed for %s.' % (view_method, ptype))
 
@@ -114,7 +114,7 @@ def resetViews(context):
     for ptype in BLOG_TYPES:
         for view_method in BLOG_VIEWS:
             type_info = portal_types.getTypeInfo(ptype)
-            if view_method in type_info.view_methods:
+            if type_info and view_method in type_info.view_methods:
                 type_info.view_methods = tuple([vm for vm in type_info.view_methods if vm !=view_method])
                 log.info('"%s" view uninstalled for %s.' % (view_method, ptype))
     
